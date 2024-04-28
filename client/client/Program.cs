5504c766f39d5dd064dd51847c6c0fe56eaebc34
IraPavlikha <148654091+IraPavlikha@users.noreply.github.com>
2024-04-28 15:10:12 +0300

    Add files via upload

---
+++ client/client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class CurrencyClient
+{
+    static void Main(string[] args)
+    {
+        string serverIP = "127.0.0.1";
+        int serverPort = 1234;
+        TcpClient client = new TcpClient(serverIP, serverPort);
+        NetworkStream stream = client.GetStream();
+        Console.WriteLine("Введіть назву валюти для співвідношення через '_' (USD_EUR|EUR_USD):");
+        string request = Console.ReadLine();
+        byte[] buffer = Encoding.ASCII.GetBytes(request);
+        stream.Write(buffer, 0, buffer.Length);
+        byte[] responseBuffer = new byte[1024];
+        int bytesRead = stream.Read(responseBuffer, 0, responseBuffer.Length);
+        string response = Encoding.ASCII.GetString(responseBuffer, 0, bytesRead);
+        Console.WriteLine("Курс валюти " + response);
+        client.Close();
+    }
+}
